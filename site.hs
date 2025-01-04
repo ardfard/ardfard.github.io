b12d1e11288e5e9ab7eaab1dc94dfa795fc4bf4f
Ardin Fardiansyah <ardfarde@gmail.com>
2025-01-04 23:30:34 +0700

    add deploy action

--- site.hs
+++ site.hs
@@ -14,6 +14,10 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
+    match "js/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match (fromList ["resume.markdown", "about.markdown"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
